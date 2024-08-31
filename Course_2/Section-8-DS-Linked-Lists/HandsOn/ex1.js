c02be65783f8b25f2dd24c625e221d606f9e517a
Slava <urigami2010@gmail.com>
2024-08-31 19:17:18 +0300

    continued a bit with linked lists

--- Course_2/Section-8-DS-Linked-Lists/HandsOn/ex1.js
+++ Course_2/Section-8-DS-Linked-Lists/HandsOn/ex1.js
@@ -57,8 +57,40 @@ class LinkedList {
     }
 
     insert(index,value){
+        if (index >= this.length) {
+            return this.append(value);
+        };
 
+        const newNode = new Node(value);
+        
+        const leader = this.traversToIndex(index-1)
+        const holdingPointer = leader.next;
+        leader.next = newNode;
+        newNode.next = holdingPointer;
+        this.length++;
+
+        return this
     }
+
+    remove(index){
+        const leader = this.traversToIndex(index -1);
+        const unwantedNode = leader.next;
+        leader.next = unwantedNode.next;
+        this.length--;
+        return this
+    }
+
+    traversToIndex(index) {
+        let counter = 0;
+        let currentNode = this.head;
+        while (counter !== index) {
+            currentNode = currentNode.next;
+            counter++;
+
+        }
+        return currentNode;
+    }
+
     printList() {
         const valuesList = [];
         let currentNode = this.head;
@@ -67,7 +99,6 @@ class LinkedList {
             currentNode = currentNode.next;
         }
         console.log(valuesList)
-        
     }
 
 }
@@ -77,6 +108,5 @@ const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(15);
 myLinkedList.prepend(25);
-
-
+myLinkedList.insert(2,99);
 myLinkedList.printList();
