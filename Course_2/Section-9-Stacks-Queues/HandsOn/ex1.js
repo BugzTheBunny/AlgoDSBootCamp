0cee4978b9c7302e6a9809323424c8a846e3dfe2
Slava <urigami2010@gmail.com>
2024-09-02 19:16:26 +0300

    Started with stacks

---
+++ Course_2/Section-9-Stacks-Queues/HandsOn/ex1.js
@@ -0,0 +1,54 @@
+// Stacks implementation
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    }
+
+    peek(){
+        return this.top;
+    }
+
+    push(value){
+        const newNode = new Node(value);
+        if (this.length == 0) { // If the stack is empty.
+            this.top = newNode; // Setting both to be the new node
+            this.bottom = newNode; // Setting both to be the new node
+        } else { // If the stack is not empty.
+            const holdingPointer = this.top; // Creating a pointer to save the top.
+            this.top = newNode; // Setting the top the new node.
+            this.top.next = holdingPointer; // moving the previous node, to the next of the new node.
+        }
+        this.length++;
+        return this;
+    }
+
+    pop(){
+        if (!this.top) return null
+        
+        // Checking if its the last node
+        if (this.top === this.bottom){
+            this.bottom = null;
+        }
+        // Setting the one node above down.
+        this.top = this.top.next;
+        this.length--;
+        return this;
+    }
+}
+
+const myStack = new Stack();
+myStack.push('google');
+myStack.push('udemy');
+myStack.push('youtube');
+
+
+console.log(myStack.peek())
---
+++ Course_2/Section-9-Stacks-Queues/README.md
@@ -0,0 +1,24 @@
+# Stacks / Queues.
+
+## Stacks (LIFO) - Last In First Out
+    ```
+    lookup -> O(n)
+    pop    -> O(1)
+    push   -> O(1)
+    peek   -> O(1)
+    ```
+
+- `Pros`
+
+- `Cons`
+
+### Examples: 
+- [Stacks](./HandsOn/ex1.js)
+
+## Queues (FIFO) - First In First Out
+    ```
+    lookup  -> O(n)
+    enqueue -> O(1)
+    dequeue -> O(1)
+    peek    -> O(1)
+    ```
